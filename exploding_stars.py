e08a91e632cdeb148ca0ae15474c66282c034726
AcheronEiden <alhassan0000@yahoo.com>
2022-07-22 00:25:10 +0200

    Update v1
    Wrote a code that forms circular shapes in random locations. The problem I encountered is that the code for removing the used random locations and colors for the shape ain't getting removed from the respective lists.

--- exploding_stars.py
+++ exploding_stars.py
@@ -1,17 +1,43 @@
-# this is for testing code snippets
+import turtle as t
 import random as r
 
-for i in range(401):
+t.speed(0)
+t.hideturtle()
+t.bgcolor("black")
+
+a = 0
+b = 0
+
+for i in range(22):
+    
     x_locations = list(range(1,401))
     y_locations = list(range(1,401))
     colors = ["red", "blue", "white", "green", "orange", "yellow", "teal", "magenta"]
-
+    
     x_location = r.choice(x_locations)
     y_location = r.choice(y_locations)
     color = r.choice(colors)
 
+    t.penup()
+    t.goto(x_location, y_location)
+    t.pendown()
+    t.color(color)
+
+    a = 0
+    b = 0
+
+    while True:
+        t.fd(a)
+        t.right(b)
+
+        a += 3
+        b += 1
+
+        if b == 110:
+            break
+    
     x_locations = x_locations.remove(x_location)
     y_locations = y_locations.remove(y_location)
     colors = colors.remove(color)
 
-print(x_locations, y_locations, colors)
+t.done()
